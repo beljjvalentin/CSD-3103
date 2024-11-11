77bcbaed916dce76d9face3ab278fa6be5927b83
beljjvalentin <153101222+LCF2345@users.noreply.github.com>
2024-11-10 22:17:11 -0400

    Adding MONGODB_URI to database.js

--- config/database.js
+++ config/database.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 // Use environment variables for credentials
 const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // MongoDB URI with database name specified
-const mongoDB = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.flk7i.mongodb.net/shop_db?retryWrites=true&w=majority&appName=Cluster0`;
+const mongoDB = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_URI}?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Connect to MongoDB
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
